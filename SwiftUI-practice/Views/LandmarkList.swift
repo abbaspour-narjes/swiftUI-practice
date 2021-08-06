b874f857140b33dfd815d812641eca0e9b597e0d
narjes abbaspour <abbaspour_narjes@hotmail.com>
2021-08-06 21:22:57 +0430

    added filter favorites with animation

--- SwiftUI-practice/Views/LandmarkList.swift
+++ SwiftUI-practice/Views/LandmarkList.swift
@@ -8,18 +8,30 @@
 import SwiftUI
 
 struct LandmarkList: View {
+    @State private var showOnlyFav:Bool  = false
+    
+    var filteredLandmarks : [Landmark] {
+        return landmarks.filter{ !showOnlyFav || $0.isFavorite }
+    }
+    
+    
     var body: some View {
-        NavigationView{
-            List(landmarks,id: \.self){ l in
-                NavigationLink(destination: LandmarkDetail(landmark: l)) {
-                    LandmarkRow(landmark: l)
-                }
-            }.listStyle(.plain)
-                .navigationTitle("Landmarks")
-                .navigationBarTitleDisplayMode(.inline)
-            
-        }
         
+        NavigationView{
+            List{
+                Toggle("Favorites only",isOn: $showOnlyFav)
+                ForEach(filteredLandmarks,id: \.self) { landmark in
+                    NavigationLink(destination: LandmarkDetail(landmark: landmark)) {
+                        LandmarkRow(landmark: landmark)
+                    }
+                }.listStyle(.plain)
+                    .navigationTitle("Landmarks")
+                    .navigationBarTitleDisplayMode(.large)
+                
+            }.listStyle(.inset)
+            .animation(.spring().delay(3.0))
+        
+        }
     }
 }
 
--- SwiftUI-practice/Views/LandmarkRow.swift
+++ SwiftUI-practice/Views/LandmarkRow.swift
@@ -17,13 +17,19 @@ struct LandmarkRow: View {
                 .frame(width: 50, height: 50)
             Text(landmark.name)
             Spacer()
+            if landmark.isFavorite {
+                Image(systemName: "star.fill")
+                    .padding()
+                    .foregroundColor(.yellow)
+            }
         }
+        
     }
 }
 
 struct LandmarkRow_Previews: PreviewProvider {
     static var previews: some View {
         LandmarkRow(landmark: landmarks[0])
-           
+        
     }
 }
